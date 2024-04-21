2bd34c2c9dbb3620df39d6ea6d518f4cc5b44a0d
maurodangelo <maurodangelo@users.noreply.github.com>
2024-04-21 18:15:02 +0200

    Added description

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@ MIT License
 Copyright (c) [2024] 
 Organization: Perlatecnica APS ETS
 Author: Mauro D'Angelo
+Example to use the LSM6DSV16X library with Sensor Fusion Low Power
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
